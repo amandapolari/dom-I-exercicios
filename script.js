f9804b6d1f7eca597bbab98eea6d183e1b075864
Amanda <amandaoliveirapolari@gmail.com>
2023-04-24 22:32:46 -0300

    Exercícios prontos

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const frutas = ["laranja", "limão", "uva"]
+// 1.
+const frutas = ['laranja', 'limão', 'uva'];
+
+const primeiraFruta = document.getElementById('fruta-1');
+const segundaFruta = document.getElementById('fruta-2');
+const terceiraFruta = document.getElementById('fruta-3');
+
+primeiraFruta.innerHTML = frutas[0];
+segundaFruta.innerHTML = frutas[1];
+terceiraFruta.innerHTML = frutas[2];
+
+// 2.
+const itemAdicional = document.getElementById('item-adicional');
+const imprimirDigitacaoConsole = () => {
+    console.log(itemAdicional.value);
+};
+
+// 3.
+const quartaFruta = document.getElementById('fruta-4');
+const botaoAdicionar = document.getElementById('btn');
+
+const adicionarConteudo = () => {
+    quartaFruta.innerHTML = itemAdicional.value;
+};
